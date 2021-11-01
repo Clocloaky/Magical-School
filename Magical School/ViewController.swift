e1200175ad623863fa8a7e8f0e5065e4e37cdda5
Irene <ijiang200292@hotmail.com>
2021-10-31 22:35:27 -0700

    Initial Commit

---
+++ Magical School/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magical School
+//
+//  Created by Irene J on 10/31/21.
+//  Copyright © 2021 Irene J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
