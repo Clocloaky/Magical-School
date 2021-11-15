0dd4e143fc71422290a1db029b0d4df99b2c923d
Irene <ijiang200292@hotmail.com>
2021-11-15 13:39:47 -0800

    finished first page of character creation
    code not inputted yet

---
+++ Magical School/StudentProfileCreationView.swift
@@ -0,0 +1,20 @@
+//
+//  StudentProfileCreationView.swift
+//  Magical School
+//
+//  Created by Irene J on 11/8/21.
+//  Copyright © 2021 Irene J. All rights reserved.
+//
+
+import UIKit
+
+class StudentProfileCreation: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
