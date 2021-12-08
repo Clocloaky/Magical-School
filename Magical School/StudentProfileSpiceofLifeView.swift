7ffe6c03faf27b959e060fff86c0f41b4588bcea
Irene <ijiang200292@hotmail.com>
2021-12-08 14:23:10 -0800

    fk it, its not working

--- Magical School/StudentProfileSpiceofLifeView.swift
+++ Magical School/StudentProfileSpiceofLifeView.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class characterTraits {
     var sectionName: String?
-    var listOfTraits: [String]?
+    var listOfTraits: [String : Bool]?
     var isChecked: Bool = false
     
-    init(sectionName: String, listOfTraits: [String]) {
+    init(sectionName: String, listOfTraits: [String : Bool]) {
         self.sectionName = sectionName
         self.listOfTraits = listOfTraits
     }
@@ -27,9 +27,10 @@ class CharacterCreationTraitsViewController: UIViewController{
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        yourTraits.append(characterTraits.init(sectionName: "How would you describe yourself?", listOfTraits: ["Shy", "Kind", "Pretty", "Cunning", "Intelligent", "Brave"]))
-        yourTraits.append(characterTraits.init(sectionName: "Which item appeals to you?", listOfTraits: ["Needle", "Diary", "Chocolate", "Roses", "Guitar", "Doll", "Necklace"]))
-        yourTraits.append(characterTraits.init(sectionName: "What are you afraid of?", listOfTraits: ["Heights", "Ghosts", "Nothing :)", "Spiders", "The Dark"]))
+        yourTraits.append(characterTraits.init(sectionName: "How would you describe yourself?", listOfTraits: ["Shy" : false, "Kind" : false, "Pretty" : false]))
+        yourTraits.append(characterTraits.init(sectionName: "Which item appeals to you?", listOfTraits: ["Needle" : false, "Diary" : false, "Chocolate" : false, "Roses" : false]))
+        /*
+         yourTraits.append(characterTraits.init(sectionName: "What are you afraid of?", listOfTraits: ["Heights", "Ghosts", "Nothing :)", "Spiders", "The Dark"]))*/
     }
 }
 
@@ -44,10 +45,13 @@ extension CharacterCreationTraitsViewController: UITableViewDataSource, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let traitIndex = ["Shy", "Kind", "Pretty", "Needle", "Dairy", "Chocolate", "Roses"]
         let item = yourTraits[indexPath.section]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = item.listOfTraits?[indexPath.row]
-        cell.accessoryType = item.isChecked ? .checkmark : .none
+        cell.textLabel?.text = traitIndex[indexPath.row]
+        
+        //cell.textLabel?.text = item.listOfTraits?[indexPath.row]
+        cell.accessoryType = (item.listOfTraits?[traitIndex[indexPath.row]])! ? .checkmark : .none
         return cell
     }
     
@@ -59,6 +63,7 @@ extension CharacterCreationTraitsViewController: UITableViewDataSource, UITableV
         tableView.deselectRow(at: indexPath, animated: true)
         let item = yourTraits[indexPath.row]
         item.isChecked = !item.isChecked
+        print(item)
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
